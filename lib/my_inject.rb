ddda7c6004f389093ad5d7ba94743334db6db7ff
Berta-G <berta.gutierrez@gmail.com>
2014-01-31 12:36:01 +0000

    calling on array of 1 test passes

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,6 +1,7 @@
 class Array 
 
 	def my_inject(total=nil, &block)
+		total ||= self.shift
 		return total if self.empty?
 	end
 
--- spec/my_inject_spec.rb
+++ spec/my_inject_spec.rb
@@ -6,6 +6,9 @@ describe "my_inject" do
 		expect([].my_inject).to eq(nil)
 	end
 
-	
+	it "returns the element of if called on an array of 1" do
+		expect([2].my_inject{|t, n| t + n}).to eq(2)
+	end
+
 
 end
