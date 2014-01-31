ff524b01594e215f0dbf57d31dd666874166a750
Berta-G <berta.gutierrez@gmail.com>
2014-01-31 12:46:59 +0000

    test added and small refactoring

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -3,8 +3,7 @@ class Array
 	def my_inject(total=nil, &block)
 		total ||= self.shift
 		return total if self.empty?
-		total = yield(total, self.shift)
-		self.my_inject(total, &block)
+		self.my_inject(yield(total, self.shift), &block)
 	end
 
 end
--- spec/my_inject_spec.rb
+++ spec/my_inject_spec.rb
@@ -14,9 +14,17 @@ describe "my_inject" do
 		expect([1,2,3].my_inject{|t, n| t + n}).to eq(6)
 	end
 
+	it "can work with array of strings" do 
+		expect(["a", "b", "c"].my_inject {|t, e| t << e} ). to eq("abc")
+	end
+
 context "total is initialized" do 
 	it "can initialize the value if the argument is passed" do
 		expect([1,2,3].my_inject(10) {|t, n| t + n }).to eq(16)
+	end
+
+	it "can return an array if initialized to empty array" do
+		expect([1,2,3,4,5].my_inject([]) {|t, n| t << n*2 }).to eq([2,4,6,8,10])
 	end
 end
 
